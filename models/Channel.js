2e139918d936487ea10a142c38dd3dbf62647081
Thang Trinh <thangtrinh@MacBook-Pro.local>
2016-09-28 11:37:08 +0700

    update model channel

--- models/Channel.js
+++ models/Channel.js
@@ -3,12 +3,13 @@ var Sequelize = require('sequelize')
 
 module.exports = function(sequelize) {
   var Channel = sequelize.define('Channel', {
+    id          : {type: Sequelize.STRING, primaryKey: true},
     name        : Sequelize.STRING,
     icon        : Sequelize.STRING,
     channel     : Sequelize.STRING
   }, {
     charset     : 'utf8mb4',
-    collate     :'utf8mb4_bin'
+    collate     : 'utf8mb4_bin'
   })
   return Channel
 }
