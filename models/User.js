c6ed2cc89f23a2987a9de9a597370a3e3290006d
Thang Trinh <thangtrinh@MacBook-Pro.local>
2016-10-11 15:54:55 +0700

    limit username length to 50 due to mysql key's length limit

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ var Sequelize = require('sequelize')
 module.exports = function(sequelize) {
   var User = sequelize.define('User', {
     password: Sequelize.STRING,
-    username: { type: Sequelize.STRING, primaryKey: true },
+    username: { type: Sequelize.STRING(50), primaryKey: true },
   }, {
     charset     :'utf8mb4',
     collate     :'utf8mb4_bin'
