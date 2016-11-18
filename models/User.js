6f57447a2b9dfb94365b915f856e13ce4f47674a
Thang Trinh <thangtrinh@MacBook-Pro.local>
2016-11-18 14:45:31 +0700

    add more detail about users: level, name, image

--- models/User.js
+++ models/User.js
@@ -1,8 +1,11 @@
 var Sequelize = require('sequelize')
 
-module.exports = function(sequelize) {
+module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define('User', {
     password: Sequelize.STRING,
+    level: DataTypes.ENUM('spector','editor','admin','god'),
+    name: Sequelize.STRING,
+    image: Sequelize.STRING,
     username: { type: Sequelize.STRING(50), primaryKey: true },
   }, {
     charset     :'utf8mb4',
